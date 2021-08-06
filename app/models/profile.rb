a967870304bf2fd069881d544e6dd229d884ba85
NicholasSaltis <gcas012107@coderacademy.edu.au>
2021-08-06 15:10:14 +1000

    changed link_to routing for profile and products show and index pages. added conditional functionality for sign-in, sign-out navbar buttons

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one :address
-  has_many :products
+  has_many :products, dependent: :destroy
   accepts_nested_attributes_for :address
 end
